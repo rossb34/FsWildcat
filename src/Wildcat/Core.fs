ad789c68d0e9d5bf4e1a524659f2b3b864209350
ross <rossbennett34@gmail.com>
2025-11-08 07:43:46 -0600

    add top of book message type

--- src/Wildcat/Core.fs
+++ src/Wildcat/Core.fs
@@ -1,15 +1,20 @@
-﻿namespace Wildcat
+﻿namespace Wildcat.Core
+
+open System
 
 
+type Symbol = Symbol of string
+type Exchange = Exchange of string
 type Price = Price of float
 type Quantity = Quantity of float
+type DateTimeUtc = DateTimeUtc of DateTimeOffset
 
 
 module PriceLevel =
     type T =
         { Price: Price
           Quantity: Quantity
-          Count: uint32 }
+          Count: int32 }
 
     /// <summary>
     /// Creates a price level.
@@ -28,4 +33,17 @@ module PriceLevel =
     let createDefault () =
         { Price = Price nan
           Quantity = Quantity 0.0
-          Count = uint32 0 }
+          Count = int32 0 }
+
+
+module Messages =
+
+    module TopOfBookMessage =
+        // TODO: replace Symbol with InstrumentId and Exchange with ExchangeId
+        type T =
+            { Symbol: Symbol
+              SequenceNumber: uint64
+              TransactTime: DateTimeUtc
+              Exchange: Exchange
+              BestBid: PriceLevel.T option
+              BestAsk: PriceLevel.T option }
