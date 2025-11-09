fe85014d55812f2bd3855b2143f6da4d854ab493
ross <rossbennett34@gmail.com>
2025-11-09 12:17:53 -0600

    add coinbase rest api and price feed
    - coinbase rest api to get l1 book snapshots
    - coinbase price feed
    - add parser module for parsing string to type

--- src/Wildcat/Core.fs
+++ src/Wildcat/Core.fs
@@ -63,3 +63,118 @@ module TopOfBook =
           Exchange = exchange
           BestBid = bestBid
           BestAsk = bestAsk }
+
+
+module Parser =
+    open System.Globalization
+
+
+    /// <summary>
+    /// Parses the string representation of a number to a double-precision floating-point number.
+    /// </summary>
+    /// <param name="s">A string of the value to parse.</param>
+    /// <returns>Returns <c>Some</c> with the value if successful otherwise <c>None</c>.</returns>
+    let tryParseFloat (s: string) =
+        let success, value = Double.TryParse s
+
+        match success with
+        | true -> Some value
+        | false -> None
+
+
+    /// <summary>
+    /// Parses the string representation of a number to a 32-bit signed integer.
+    /// </summary>
+    /// <param name="s">A string of the value to parse.</param>
+    /// <returns>Returns <c>Some</c> with the value if successful otherwise <c>None</c>.</returns>
+    let tryParseInt32 (s: string) =
+        let success, value = Int32.TryParse s
+
+        match success with
+        | true -> Some value
+        | false -> None
+
+
+    /// <summary>
+    /// Parses the string representation of a number to a 64-bit signed integer.
+    /// </summary>
+    /// <param name="s">A string of the value to parse.</param>
+    /// <returns>Returns <c>Some</c> with the value if successful otherwise <c>None</c>.</returns>
+    let tryParseInt64 (s: string) =
+        let success, value = Int64.TryParse s
+
+        match success with
+        | true -> Some value
+        | false -> None
+
+
+    /// <summary>
+    /// Parses the string representation of a number to a 32-bit unsigned integer.
+    /// </summary>
+    /// <param name="s">A string of the value to parse.</param>
+    /// <returns>Returns <c>Some</c> with the value if successful otherwise <c>None</c>.</returns>
+    let tryParseUInt32 (s: string) =
+        let success, value = UInt32.TryParse s
+
+        match success with
+        | true -> Some value
+        | false -> None
+
+
+    /// <summary>
+    /// Parses the string representation of a number to a 64-bit signed integer.
+    /// </summary>
+    /// <param name="s">A string of the value to parse.</param>
+    /// <returns>Returns <c>Some</c> with the value if successful otherwise <c>None</c>.</returns>
+    let tryParseUInt64 (s: string) =
+        let success, value = UInt64.TryParse s
+
+        match success with
+        | true -> Some value
+        | false -> None
+
+
+    /// <summary>
+    /// Parses the string representation of a logical value to a boolean.
+    /// </summary>
+    /// <param name="s">A string of the value to parse.</param>
+    /// <returns>Returns <c>Some</c> with the value if successful otherwise <c>None</c>.</returns>
+    let tryParseBool (s: string) =
+        let success, value = Boolean.TryParse s
+
+        match success with
+        | true -> Some value
+        | false -> None
+
+
+    /// <summary>
+    /// Parses the string representation of a timestamp to a ``DateTimeUtc``.
+    /// </summary>
+    /// <param name="s">A string of the time to parse. The input string must be an ISO 601 formatted timestamp with microsecond precision.</param>
+    /// <returns>Returns <c>Some</c> with the value if successful otherwise <c>None</c>.</returns>
+    let tryParseDateTimeUtcMicros (s: string) =
+        let fmt = "yyyy-MM-ddTHH:mm:ss.ffffffZ"
+
+        let success, value = DateTimeOffset.TryParseExact(s, fmt, null, DateTimeStyles.None)
+
+        match success with
+        | true -> Some(DateTimeUtc value)
+        | false -> None
+
+
+    /// <summary>
+    /// Drops the nanos portion from the time string.
+    /// </summary>
+    /// <param name="s">The input time string</param>
+    /// <remarks>
+    /// This function expects an input time string in ISO 8601 format with nanosecond precision.
+    /// Example: 2025-10-28T02:50:49.965682759Z -> 2025-10-28T02:50:49.965682Z
+    /// </remarks>
+    /// <returns>Returns <c>Some</c> with the  time string with microsecond precision, otherwise <c>None</c></returns>
+    let tryDropNanos s =
+        let len = String.length s
+
+        match len with
+        | 30 -> Some(s[0..25] + "Z")
+        | 27 -> Some s
+        | _ -> None
